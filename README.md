632ec1e677884563fb898ef3b20cb6243ddf610b
Michal Zaborowski <michal@zaborowski.info.pl>
2020-08-13 16:27:20 +0200

    talents->themes + cleaning + better README

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ used by over 22 milion people around the world. So there is a lot of data to be 
 
 Aim for this repository is to contain my tries on notebooks. 
 
+To see fast preview - [sample demography](./sample_demography.html), [sample themes](./sample_themes.html).
+
+You can run notebook with binder. Here is the link:
+https://mybinder.org/v2/gh/TeXXaS/gallup-themes/master?filepath=world-themes.ipynb
+
 # Legal part.
 Names of themes, name Strengths Finder are property of Gallup. 
 All the data is owned by Gallup.
--- refs.py
+++ refs.py
@@ -1,28 +1,28 @@
 
 DOMAINS = {
     'Executing': {
-        'talents': frozenset(("Achiever", "Arranger", "Belief", "Consistency", "Deliberative", "Discipline", "Focus",
+        'themes': frozenset(("Achiever", "Arranger", "Belief", "Consistency", "Deliberative", "Discipline", "Focus",
                        "Responsibility", "Restorative")),
         'description': "Executing",
         "color": "#5c396d"
     },
 
     'Influencing': {
-        'talents': frozenset(("Activator", "Command", "Communication", "Competition", "Maximizer", "Self-Assurance",
+        'themes': frozenset(("Activator", "Command", "Communication", "Competition", "Maximizer", "Self-Assurance",
                        "Significance", "Woo")),
         'description': "Influencing",
         'color': "#f6b333"
     },
 
     'Relationship building': {
-        'talents': frozenset(("Adaptability", "Connectedness", "Developer", "Empathy", "Harmony", "Includer",
+        'themes': frozenset(("Adaptability", "Connectedness", "Developer", "Empathy", "Harmony", "Includer",
                        "Individualization", "Positivity", "Relator")),
         'description': "Relationship building",
         'color': "#1f4b74"
     },
 
     'Strategic thinking': {
-        'talents': frozenset(("Analytical", "Context", "Futuristic", "Ideation", "Input", "Intellection", "Learner",
+        'themes': frozenset(("Analytical", "Context", "Futuristic", "Ideation", "Input", "Intellection", "Learner",
                        "Strategic")),
         'description': "Strategic thinking",
         'color': "#9c0000"
